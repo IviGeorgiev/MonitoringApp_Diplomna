764107d8270ee0086f74c19e215d84e4257bcb49
Ivaylo Georgiev <85507998+IviGeorgiev@users.noreply.github.com>
2022-12-20 21:29:23 +0200

    adding AddObservation screen

---
+++ app/src/main/java/com/example/monitoringapp/AddObservation.kt
@@ -0,0 +1,11 @@
+package com.example.monitoringapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddObservation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_observation_screen)
+    }
+}
