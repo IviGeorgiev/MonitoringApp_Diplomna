3468448fe1fd7dc0e7f17b7b84aa0d32827fb4ba
Ivaylo Georgiev <85507998+IviGeorgiev@users.noreply.github.com>
2023-04-19 16:05:24 +0300

    Some UI changes, filter my observations

--- app/src/main/java/com/example/monitoringapp/views/ObservationsFragment.kt
+++ app/src/main/java/com/example/monitoringapp/views/ObservationsFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
@@ -14,6 +15,7 @@ import com.example.monitoringapp.adapter.ObservationsRecyclerAdapter
 import com.example.monitoringapp.model.ObservationData
 import com.example.monitoringapp.databinding.FragmentObservationsBinding
 import com.example.monitoringapp.viewmodel.ObservationsViewModel
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 
@@ -29,13 +31,36 @@ class ObservationsFragment : Fragment() {
     ): View {
         binding = FragmentObservationsBinding.inflate(layoutInflater, container, false)
 
+        //Filter the users observations
+        binding.myFilterFabButton.setOnClickListener{
+            val currentUser = FirebaseAuth.getInstance().currentUser
+            val userEmail = currentUser?.email
+
+            if (userEmail != null) {
+                val query = FirebaseFirestore.getInstance().collection("observations")
+                    .whereEqualTo("creator", userEmail)
+
+                query.get().addOnSuccessListener { querySnapshot ->
+                    val filteredObservations = querySnapshot.toObjects(ObservationData::class.java)
+                    Log.d("ObservationsFragment", "Filtered observations: $filteredObservations")
+                    adapter.updateObservations(filteredObservations)
+                    adapter.notifyDataSetChanged()
+                    Toast.makeText(requireContext(), "My observations", Toast.LENGTH_SHORT).show()
+                }.addOnFailureListener { exception ->
+                    Log.e("ObservationsFragment", "Error filtering observations", exception)
+                }
+            }
+        }
+
+        //Sort by species count
         binding.filterFabButton.setOnClickListener{
             val query = FirebaseFirestore.getInstance().collection("observations")
                 .orderBy("species", Query.Direction.DESCENDING)
+
             query.get().addOnSuccessListener { querySnapshot ->
                 val sortedObservations = querySnapshot.toObjects(ObservationData::class.java)
                     .sortedByDescending { it.species.toIntOrNull() }
-                Log.d("ObservationsFragment", "Filtered observations: $sortedObservations")
+                Log.d("ObservationsFragment", "Sorted observations: $sortedObservations")
                 adapter.updateObservations(sortedObservations)
                 adapter.notifyDataSetChanged()
             }.addOnFailureListener { exception ->
